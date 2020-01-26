c485e7583f008c0f59fea2434006cbaebac8c83e
doingodswork <doingodswork@protonmail.com>
2020-01-26 18:30:51 +0100

    Refactor imdb2torrent cache

---
+++ pkg/imdb2torrent/cache.go
@@ -0,0 +1,19 @@
+package imdb2torrent
+
+type cache struct {
+	m map[string][]Result
+}
+
+func newCache() *cache {
+	return &cache{
+		m: map[string][]Result{},
+	}
+}
+
+func (c cache) set(imdbID string, magnets []Result) {
+	c.m[imdbID] = magnets
+}
+
+func (c cache) get(imdbID string) []Result {
+	return c.m[imdbID]
+}
--- pkg/imdb2torrent/client.go
+++ pkg/imdb2torrent/client.go
@@ -8,7 +8,7 @@ import (
 
 type Client struct {
 	httpClient *http.Client
-	cache      map[string][]Result
+	cache      *cache
 }
 
 func NewClient(timeout time.Duration) Client {
@@ -16,21 +16,25 @@ func NewClient(timeout time.Duration) Client {
 		httpClient: &http.Client{
 			Timeout: timeout,
 		},
-		cache: make(map[string][]Result),
+		cache: newCache(),
 	}
 }
 
 // FindMagnets tries to find magnet URLs for the given IMDb ID.
 // It only returns 720p and 1080p videos.
+// It caches results once they're found.
+// It can return an empty slice and no error if no actual error occurred (for example if torrents where found but no >720p videos).
 func (c Client) FindMagnets(imdbID string) ([]Result, error) {
 	// Check cache first
-	if results, ok := c.cache[imdbID]; ok {
+	if results := c.cache.get(imdbID); len(results) > 0 {
 		return results, nil
 	}
 
 	results, err := c.checkYTS(imdbID)
 	if err != nil {
 		log.Println("No torrents found on YTS:", err)
+	} else if len(results) == 0 {
+		log.Println("No torrents found on YTS")
 	}
 
 	return results, err
