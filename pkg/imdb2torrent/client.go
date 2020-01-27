29d8c3e2f636836a2f8455bb7a16b30e1cccdaed
doingodswork <doingodswork@protonmail.com>
2020-01-27 17:36:03 +0100

    Improve performance by parallelizing torrent search

--- pkg/imdb2torrent/client.go
+++ pkg/imdb2torrent/client.go
@@ -1,8 +1,10 @@
 package imdb2torrent
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -30,38 +32,79 @@ func (c Client) FindMagnets(imdbID string) ([]Result, error) {
 		return results, nil
 	}
 
-	combinedResults := []Result{}
+	torrentSiteCount := 2
+	resChan := make(chan []Result, torrentSiteCount)
+	errChan := make(chan error, torrentSiteCount)
 
 	// YTS
-	results, err := c.checkYTS(imdbID)
-	if err != nil {
-		log.Println("Couldn't find torrents on YTS:", err)
-	} else if len(results) == 0 {
-		log.Println("No torrents found on YTS")
-	} else {
-		combinedResults = append(combinedResults, results...)
-	}
+	go func() {
+		results, err := c.checkYTS(imdbID)
+		if err != nil {
+			log.Println("Couldn't find torrents on YTS:", err)
+			errChan <- err
+		} else {
+			if len(results) == 0 {
+				log.Println("No torrents found on YTS")
+			}
+			resChan <- results
+		}
+	}()
 
 	// TPB
-	results, err = c.checkTPB(imdbID)
-	if err != nil {
-		log.Println("Couldn't find torrents on TPB:", err)
-	} else if len(results) == 0 {
-		log.Println("No torrents found on TPB")
-	} else {
-		combinedResults = append(combinedResults, results...)
-	}
+	go func() {
+		results, err := c.checkTPB(imdbID)
+		if err != nil {
+			log.Println("Couldn't find torrents on TPB:", err)
+			errChan <- err
+		} else {
+			if len(results) == 0 {
+				log.Println("No torrents found on TPB")
+			}
+			resChan <- results
+		}
+	}()
 
 	// TODO: Check other torrent sites
 
-	// Remove duplicates
-	noDupResults := []Result{}
-	infoHashes := map[string]struct{}{}
-	for _, result := range combinedResults {
-		if _, ok := infoHashes[result.InfoHash]; !ok {
-			noDupResults = append(noDupResults, result)
-			infoHashes[result.InfoHash] = struct{}{}
+	combinedResults := []Result{}
+	errs := []error{}
+	dupRemovalRequired := false
+	for i := torrentSiteCount; i > 0; i-- {
+		// No timeout for the goroutines because their HTTP client has a timeout already
+		select {
+		case err := <-errChan:
+			errs = append(errs, err)
+		case results := <-resChan:
+			combinedResults = append(combinedResults, results...)
+			if !dupRemovalRequired && len(combinedResults) > 0 && len(results) > 0 {
+				dupRemovalRequired = true
+			}
 		}
+	}
+
+	// Return error (only) if all torrent sites returned actual errors (and not just empty results)
+	if len(errs) == torrentSiteCount {
+		errsMsg := "Couldn't find torrents on any site: "
+		for i := 1; i <= torrentSiteCount; i++ {
+			errsMsg += fmt.Sprintf("%v.: %v; ", i, errs[i])
+		}
+		errsMsg = strings.TrimSuffix(errsMsg, "; ")
+		return nil, fmt.Errorf(errsMsg)
+	}
+
+	// Remove duplicates.
+	// Only necessary if we got non-empty results from more than one torrent site.
+	noDupResults := []Result{}
+	if dupRemovalRequired {
+		infoHashes := map[string]struct{}{}
+		for _, result := range combinedResults {
+			if _, ok := infoHashes[result.InfoHash]; !ok {
+				noDupResults = append(noDupResults, result)
+				infoHashes[result.InfoHash] = struct{}{}
+			}
+		}
+	} else {
+		noDupResults = combinedResults
 	}
 
 	if len(noDupResults) <= 0 {
@@ -70,7 +113,7 @@ func (c Client) FindMagnets(imdbID string) ([]Result, error) {
 		c.cache.set(imdbID, noDupResults)
 	}
 
-	return noDupResults, err
+	return noDupResults, nil
 }
 
 type Result struct {
