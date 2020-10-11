517a84d38fc627704fa9012144997f4de5298e62
doingodswork <doingodswork@protonmail.com>
2020-10-11 13:04:30 +0200

    Improve build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,13 +4,13 @@
 # It requires Go to be installed already.
 # It doesn't matter what the working directory is when calling this script.
 
+set -euxo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 if [ "$#" -eq 0 ]; then
     echo "You have to pass a target operating system as argument, like windows, darwin or linux"
     exit 1
 fi
-
-set -euxo pipefail
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Create pkger files
 cd "${DIR}/.."
